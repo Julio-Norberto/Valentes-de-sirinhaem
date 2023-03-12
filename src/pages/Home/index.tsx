406636715c405e463579b3442dc2fec36a3de3f8
Julio-Norberto <juliocsscl@gmail.com>
2023-03-12 11:28:05 -0300

    configuring responsivity for tablet

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,33 @@
 import './home.css'
 
 import { Menu } from '../../components/Menu'
+import { Link } from 'react-router-dom'
+
+import dogs from '../../assets/dogsHome.svg'
 
 export const Home: React.FunctionComponent = () => {
     return (
         <div className='home-container'>
           <Menu />
+
+          <div className='home-content'>
+
+            <div className='home-text'>
+              <div>
+                <h1>Valentes de <br /> Sirinhaém</h1>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. A cumque unde cupiditate vero sunt! Voluptatem ipsa sunt sapiente beatae, harum deserunt incidunt doloremque et numquam, laboriosam inventore esse? Blanditiis, repellat?
+                </p>
+              </div>
+
+              <Link className='home-button' to='adoption' >Disponíveis para adoção</Link>
+            </div>
+
+            <div className='home-img'>
+              <img src={dogs} alt="" />
+            </div>
+
+          </div>
 
         </div>
     )
