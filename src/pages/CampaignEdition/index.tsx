572ee6eea118e40279bfae470c2e48a0257519a6
Martin <escorpmartin97@gmail.com>
2023-03-14 18:03:04 -0300

    feat: add main structure for CampaignEdition page

--- src/pages/CampaignEdition/index.tsx
+++ src/pages/CampaignEdition/index.tsx
@@ -1,9 +1,24 @@
+import { Footer } from '../../components/Footer'
+import { Menu } from '../../components/Menu'
 import './campaignEdition.css'
 
 export const CampaignEdition: React.FunctionComponent = () => {
     return (
-        <div>
-            <h1>Página Edição de campanhas</h1>
+        <div className='container'>
+          <header>
+            <Menu />
+          </header>
+    
+          <section className='content'>
+            <h1>Todas as campanhas</h1>
+    
+            <div className='pets-content'>
+              <div className='pets'></div>
+              <div className='pets'></div>
+              <div className='pets'></div>
+            </div>
+          </section>
+          <Footer />
         </div>
-    )
+      )
 }
