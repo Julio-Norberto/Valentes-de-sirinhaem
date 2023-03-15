9342662a64de74f2c8ae7e456d5875f45c4335d4
Julio-Norberto <juliocsscl@gmail.com>
2023-03-15 10:35:20 -0300

    style: remove margin-top in component bar

--- src/pages/Adoption/index.tsx
+++ src/pages/Adoption/index.tsx
@@ -1,9 +1,13 @@
+import { Bar } from '../../components/Bar'
 import './adoption.css'
 
 export const Adoption: React.FunctionComponent = () => {
     return (
-        <div>
-            <h1>Página adoção</h1>
+        <div className='adoption-container'>
+            <Bar withMenu={true} />
+            <div className='adoption-content'>
+              <h1>content</h1>
+            </div>
         </div>
     )
 }
