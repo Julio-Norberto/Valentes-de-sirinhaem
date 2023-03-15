31ded9f35363df033af9f1c634569e46583d2965
Martin <escorpmartin97@gmail.com>
2023-03-14 21:56:42 -0300

    feat: ending campaign card

--- src/components/CampaignEditionCard/index.tsx
+++ src/components/CampaignEditionCard/index.tsx
@@ -1,5 +1,20 @@
 import './campaignEditionCard.css'
 
-export const CampaignEditionCard: React.FunctionComponent = () => {
-  return <div></div>
+interface CanpaignEditionCardProps {
+  description: string
+  image: string
+}
+
+export const CampaignEditionCard: React.FunctionComponent<CanpaignEditionCardProps> = (props) => {
+  return (
+    <div className='card-container'>
+      <h2>{props.description}</h2>
+      <img src={props.image} alt={props.description} />
+
+      <div className='btn-container'>
+        <button className='btn-edit'>Editar informações</button>
+        <button className='btn-remove'>Remover pet</button>
+      </div>
+    </div>
+  )
 }
--- src/pages/CampaignEdition/index.tsx
+++ src/pages/CampaignEdition/index.tsx
@@ -1,8 +1,16 @@
+import { CampaignEditionCard } from '../../components/CampaignEditionCard'
 import { Footer } from '../../components/Footer'
 import { Menu } from '../../components/Menu'
+import dogExemple from '../../assets/dog-example2.jpg'
 import './campaignEdition.css'
 
 export const CampaignEdition: React.FunctionComponent = () => {
+    const mockPets = [
+        {description: 'Campanha para tratamento da pata do Bob', image: dogExemple, id: '123'},
+        {description: 'Campanha para tratamento da pata do Bob', image: dogExemple, id: '456'},
+        {description: 'Campanha para tratamento da pata do Bob', image: dogExemple, id: '789'},
+      ]
+
     return (
         <div className='container'>
           <header>
@@ -13,9 +21,17 @@ export const CampaignEdition: React.FunctionComponent = () => {
             <h1>Todas as campanhas</h1>
     
             <div className='pets-content'>
-              <div className='pets'></div>
-              <div className='pets'></div>
-              <div className='pets'></div>
+            {
+            mockPets.map((pet) => {
+              return (
+                <CampaignEditionCard 
+                  key={pet.id}
+                  image={pet.image}
+                  description={pet.description}
+                />
+              )
+            })
+          }
             </div>
           </section>
           <Footer />
