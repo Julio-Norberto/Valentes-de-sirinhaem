18e0b073be89d9e63893a6fc23d1864825c2280e
Julio-Norberto <juliocsscl@gmail.com>
2023-03-16 09:36:47 -0300

    feat: create page campaign and component CampaignPageCards

---
+++ src/components/CampaignPageCards/index.tsx
@@ -0,0 +1,35 @@
+import { Button } from '../Button'
+
+interface ICampaignPageCards {
+  name: string
+  title: string
+  image: any
+  description: string
+}
+
+export const CampaignPageCards: React.FunctionComponent<ICampaignPageCards> = ({title, image, description, name}: ICampaignPageCards) => {
+  return (
+    <div className='adoption-card-container'>
+
+    <div className='adoption-card-content'>
+      <div>
+        <h2 style={{ marginBottom: '24px' }} > {title} </h2>
+      </div>
+
+      <div className='card-image-adoption'>
+        <img style={{ marginBottom: '24px' }} width={200} height={200} src={image} alt="" />
+      </div>
+
+      <div className='card-text-adoption'>
+        <p style={{ width: '80%', margin: '0 auto' }} > {description} </p>
+      </div>
+
+      <div className='card-adoption-button'>
+        <Button paddingLeft='40px' paddingRight='40px' title={`Ajudar ${name}`} destination='/adoption' />
+      </div>
+
+    </div>
+
+  </div>
+  )
+}
