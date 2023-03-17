b96d5b31aeebbad2dd04af5b8810070a4ecc16eb
Julio-Norberto <juliocsscl@gmail.com>
2023-03-17 13:36:04 -0300

    feat: create login page and configure your responsivity

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,28 @@
+import { Bar } from '../../components/Bar'
+import { Footer } from '../../components/Footer'
 import './login.css'
 
 export const Login: React.FunctionComponent = () => {
-    return (
-        <div>
-            <h1>Página login</h1>
-        </div>
-    )
+  return (
+    <div className='login-container'>
+      <Bar withMenu={true} />
+
+      <div className='login-content'>
+        <form>
+          <h1>Faça login para ter <br /> acesso ao painel</h1>
+          <div className='div-inputs'>
+            <input name='login' id='login' type="text" placeholder='Digite o seu login...' />
+          </div>
+
+          <div className='div-inputs'>
+            <input type="password" name='password' id='password' placeholder='Digite sua senha...' />
+          </div>
+
+          <input className='btn-submit' type="submit" value='Fazer Login' />
+        </form>
+      </div>
+
+      <Footer />
+    </div>
+  )
 }
