051a041010cf68a8fda4986203f0f9ddbe15791e
Julio-Norberto <juliocsscl@gmail.com>
2023-03-19 10:25:29 -0300

    feat: add diferent types on modal for rendering diferent forms

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,23 +2,27 @@ import './modal.css'
 
 import { Form } from '../FormModal'
 
-export const Modal: React.FunctionComponent = () => {
+type ModalProps = {
+  typeModal: 'donation' | 'pets' | 'campaigns'
+}
 
-    function closeModal() {
-        const modal = document.querySelector('#modal')
-        modal!.classList.add('hide')
-    }
+export const Modal: React.FunctionComponent<ModalProps> = ({typeModal}: ModalProps) => {
 
-    return (
-        <div id="modal" className="hide">
-            <div className='fade' onClick={closeModal}>
+  function closeModal() {
+    const modal = document.querySelector('#modal')
+    modal!.classList.add('hide')
+  }
 
-            </div>
+  return (
+    <div id="modal" className="hide">
+      <div className='fade' onClick={closeModal}>
 
-            <div className='modal'>
-                <h2>Ajude essa campanha usando o pix <br /> através do QRCode </h2>
-                <Form />
-            </div>
-        </div>
-    )
+      </div>
+
+      <div className='modal'>
+        <h2>Ajude essa campanha usando o pix <br /> através do QRCode </h2>
+        { typeModal == 'donation' ? <Form /> : typeModal == 'pets' ? '' : '' }
+      </div>
+    </div>
+  )
 }
